42a11dd7ecd3e86d8ad663cf03a8b3080f2322bc
nitinagarwal <agarwal@uci.edu>
2019-09-02 19:54:09 -0700

    updated inference

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -3,7 +3,7 @@
 
 python inference.py \
     --dataDir ./data \
-    --cls  modelNet40_2.5k \
+    --cls  abc_2.5k \
     --model ./out/log1/best_net__.pth \
     --outf ./out/log1 \
     --type test \
